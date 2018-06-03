3ffb5efe69c5db542d058ce2714e73257e477538
Sergey Titov <sergey.sergeevich.titov@protonmail.com>
2018-06-03 20:59:59 +0300

    alpha

--- App.js
+++ App.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
+  Button,
   View
 } from 'react-native'
-import { createSwitchNavigator, createBottomTabNavigator } from 'react-navigation'
+import { createSwitchNavigator, createBottomTabNavigator, createStackNavigator } from 'react-navigation'
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 import HomeScreen from './screens/Home'
@@ -12,6 +13,45 @@ import LoginScreen from './screens/Login'
 import SearchScreen from './screens/Search'
 import StreamScreen from './screens/Stream'
 import ProfileScreen from './screens/Profile'
+
+class SongScreen extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button
+          title='Long live the Song Screen!'
+          onPress={()=> this.props.navigation.navigate('Home')}
+        />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center'
+    alignItems: 'center'
+    fles: 1,
+    backgroundColor: '#fff'
+  }
+})
+
+//Stack Navigators
+
+const HomeStack = createStackNavigator({
+  Home: HomeScreen,
+  Song: SongScreen
+})
+
+const SearchStack = createStackNavigator({
+  Search: SearchScreen,
+  Song: SongScreen
+})
+
+const ProfileStack = createStackNavigator({
+  Profile: ProfileScreen,
+  Song: SongScreen
+})
 
 const Tabs = createBottomTabNavigator({
   Home: HomeScreen,
