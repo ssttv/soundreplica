fa11e15b8affbcdfb83dde29eec37b60aa958256
Sergey Titov <sergey.sergeevich.titov@protonmail.com>
2018-06-03 07:31:40 +0300

    Warnings will be ignored now

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'ModuleRCTImageLoader'])
+
 AppRegistry.registerComponent('soundreplica', () => App);
