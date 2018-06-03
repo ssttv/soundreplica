4462224a5a70d24958d09078cc8cfdeb7f7cc58c
Sergey Titov <sergey.sergeevich.titov@protonmail.com>
2018-06-03 07:29:28 +0300

    Tab navigation implemented

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import {
   Text,
   View
 } from 'react-native'
-import { createSwitchNavigator } from 'react-navigation'
+import { createSwitchNavigator, createBottomTabNavigator } from 'react-navigation'
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 import HomeScreen from './screens/Home'
@@ -13,9 +13,44 @@ import SearchScreen from './screens/Search'
 import StreamScreen from './screens/Stream'
 import ProfileScreen from './screens/Profile'
 
+const Tabs = createBottomTabNavigator({
+  Home: HomeScreen,
+  Stream: StreamScreen,
+  Search: SearchScreen,
+  Profile: ProfileScreen
+},{
+  navigationOptions: ({ navigation }) => ({
+    tabBarIcon: ({ tintColor }) => {
+      const { routeName } = navigation.state
+      let iconName
+      if (routeName === 'Home') {
+        iconName = 'home'
+      } else if (routeName === 'Stream') {
+        iconName = 'flash'
+      } else if (routeName === 'Search') {
+        iconName = 'search'
+      } else if (routeName === 'Profile') {
+        iconName = 'account'
+      }
+      return iconName === 'search' ?
+      <MaterialIcons name={iconName} size={25} color={tintColor} /> :
+      <MaterialCommunityIcons name={iconName} size={25} color={tintColor} />
+    }
+  }),
+  tabBarOptions: {
+    activeTintColor: '#fff',
+    inactiveTintColor: 'grey',
+    activeBackgroundColor: 'grey',
+    showLabel: false,
+    style: {
+      backgroundColor: '#000'
+    }
+  }
+})
+
 export default Switch = createSwitchNavigator({
   Login: LoginScreen,
-  Home: HomeScreen
+  Home: Tabs
 },{
   initialRouteName: 'Login'
 })
